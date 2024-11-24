e188b9791d5d801ba02941040712c872761a3a58
HP <HP@LocPDT>
2024-11-24 15:12:39 +0700

    Hi

---
+++ begininng/src/batDau/MangMieng.java
@@ -0,0 +1,14 @@
+package batDau;
+
+public class MangMieng {
+	static void myMethod(String fname, int age) {
+		System.out.println("今年" + fname + "は" + age + "歳だと於みますと思う");
+	}
+	
+
+	public static void main(String[] args) {
+		myMethod("John1", 33);
+		myMethod("John2", 33);
+		myMethod("John3", 33);
+	}
+}
